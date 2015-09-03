6fd91d765a1fb978bac5d4e2f5c6505e7e85dcea
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-09-03 13:55:45 +0200

    entity.cc: gen_new_graph removed

--- entity.cc
+++ entity.cc
@@ -1,17 +1,9 @@
 #include "entity.hpp"
 #include "node.hpp"
 
-static Node* gen_new_graph()
-{
-    //Node's ctor recursively generates a tree
-    Node* root = new Node(0, ARGS);
-
-    return root;
-}
-
 Entity::Entity() : fittness{-1.0}
 {
-    root = gen_new_graph();
+    root = new Node(0, ARGS);
 }
 
 double Entity::get_fittness()
